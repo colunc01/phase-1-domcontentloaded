2a0f27489793fd4337a9b49229e35c12df2f96b6
Cesar Colunga <cesara.colunga@gmail.com>
2023-03-23 12:33:02 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    //callback function is used to target the paragraph element with id="text" and replace text with "This is really cool!"
+    const element = document.getElementById("text");
+    element.innerHTML = "This is really cool!";
+  console.log("The DOM has loaded");
+});
+
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
